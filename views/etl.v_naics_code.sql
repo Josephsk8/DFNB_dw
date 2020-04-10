aa1e5f1f5a9ca68482cd0d97ac22eff89c4b7977
Joseph <jmuerasl@ldsbc.edu>
2020-04-10 00:00:37 -0600

    IT 243 - Added LoadNAICSCodeHierDim_xx.dtsx

---
+++ views/etl.v_naics_code.sql
@@ -0,0 +1,26 @@
+--etl.v_naics_code
+
+USE [DFNB3];
+GO
+DROP VIEW etl.v_naics_code;
+GO
+
+SET ANSI_NULLS ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+
+CREATE VIEW etl.v_naics_code
+AS
+
+SELECT LEN(nc.[2017_NAICS_US_code]) AS naics_level
+,CASE
+WHEN LEN(nc.[2017_NAICS_US_code]) = 2
+THEN NULL
+ELSE LEFT(nc.[2017_NAICS_US_code], LEN(nc.[2017_NAICS_US_code]) - 1)
+END AS naics_parent_code
+,nc.[2017_NAICS_US_code] AS naics_code
+,nc.[2017_NAICS_US_title] AS naics_desc
+FROM stg.NAICS_CODES_2017 AS nc;
+
+GO
